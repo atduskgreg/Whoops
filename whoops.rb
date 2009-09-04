c150e20c0f231e24f0c8aae9500b378e98b8a796
Greg Borenstein <greg@mfdz.com>
2009-09-04 18:52:54 -0400

    sequence can close itself; better beat api

--- whoops.rb
+++ whoops.rb
@@ -1,10 +1,29 @@
 module Whoops
   class Beat
     attr_accessor :duration, :pitch
+    
+    def initialize(d, p)
+      @duration = d
+      @pitch    = p
+    end
+    
+    def generate
+      # deal with octave?
+      "#{duration}#{pitch}"
+    end
+    
+    def quarter_note_value
+      4 / duration.to_f
+    end
   end
   
   class Sequence
-    attr_accessor :current_length, :target_length, :beats
+    attr_accessor :target_length, :beats
+    
+    def initialize(opts)
+      @target_length  = opts[:target_length]
+      @beats          = []
+    end
     
     def <<(beat)
       if room_for?(beat)
@@ -12,23 +31,30 @@ module Whoops
       else
         close
       end
+      self
     end
     
     def append_beat(beat)
       beats << beat
     end
     
+    def current_length
+      result = 0
+      beats.each{|b| result += b.quarter_note_value }
+      result
+    end
+    
     def room_for?(beat)
-      current_length + beat.duration <= target_length
+      current_length + beat.quarter_note_value <= target_length
     end
     
     def open?
-      current_length < target_legnth
+      current_length < target_length
     end
     
     def close
       silent_duration = target_length - current_length
-      self.append_beat(Beat.new( :duration => silent_duration, :pitch => nil ))
+      self.append_beat(Beat.new( silent_duration.to_i * 4, nil ))
     end
   end
 end
